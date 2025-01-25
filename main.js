da36d767f9e1e3cdc975c0feb34b4612f8e8ae9e
code <amrkhmohammedyoussef@gmail.com>
2025-01-25 09:55:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,36 @@ setInterval(function () {
         counter = 1;
     }
 }, 5000);
+///////////////////////
+// فتح المودال
+function openModal() {
+    const modal = document.getElementById("profileModal");
+    const modalImage = document.getElementById("modalImage");
+
+    modal.style.display = "block";
+    modalImage.src = document.querySelector(".profile-pic").src; // تعيين صورة المودال
+}
+
+// غلق المودال
+function closeModal() {
+    document.getElementById("profileModal").style.display = "none";
+}
+
+// غلق المودال عند الضغط على الخلفية
+document.getElementById("profileModal").addEventListener("click", function (event) {
+    if (event.target === this) { // التحقق من أن المستخدم ضغط على الخلفية
+        closeModal();
+    }
+});
+////////////////////////////////////////
+function toggleAccordion(element) {
+    const content = element.nextElementSibling;
+    const icon = element.querySelector('.icon');
+
+    // إظهار أو إخفاء المحتوى
+    content.classList.toggle('open');
+
+    // تدوير الأيقونة
+    icon.classList.toggle('rotate');
+}
+/////////////////////////////////////
